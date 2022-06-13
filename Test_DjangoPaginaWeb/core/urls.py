6533ba164f4c5de6af3f3f894408f6ae2212cdf6
saitanax3000 <102157602+saitanax3000@users.noreply.github.com>
2022-06-13 14:59:33 -0400

    Agregamos index.html
    Co-Authored-By: Deducible <101644071+Deducible@users.noreply.github.com>

--- Test_DjangoPaginaWeb/core/urls.py
+++ Test_DjangoPaginaWeb/core/urls.py
@@ -1,6 +1,7 @@
+from operator import index
 from django.urls import path
-from .views import home
+from .views import index
 
 urlpatterns = [
-	path ('', home,name="home"),
+	path ('', index,name="index"),
 ]
--- Test_DjangoPaginaWeb/core/views.py
+++ Test_DjangoPaginaWeb/core/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home (request):
+def index (request):
 
-	return render(request, 'core/home.html')
+	return render(request, 'core/index.html')
